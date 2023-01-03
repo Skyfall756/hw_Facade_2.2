619398a8b1bc097c38006692fc425d86a826225e
Skyfall756 <110997330+Skyfall756@users.noreply.github.com>
2023-01-03 20:19:17 +0300

    init commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps binOps = new BinOps();
+        System.out.println(binOps.sum("01", "011"));
+        System.out.println(binOps.mult("0100011", "0100"));
+    }
+}
